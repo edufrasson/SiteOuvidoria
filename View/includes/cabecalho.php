bcb9bcb21742bfce0ecac9da505a548a0f38e4e1
Eduardo Frasson <edudomine2016@gmail.com>
2022-05-06 22:18:58 -0300

    mudanças no cabeçalho

--- View/includes/cabecalho.php
+++ View/includes/cabecalho.php
@@ -1,36 +1,41 @@
-<header class="p-3 mb-3 border-bottom">
-    <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                    <use xlink:href="#bootstrap" />
-                </svg>
-                <img src="../../img/logo.png" alt="mdo" width="52" height="52" class="d-inline-block align-text-top">
-            </a>
-
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2 link-dark"></a></li>
-                <li><a href="..\..\SiteOuvidoriaJau\MenuInicial\TelaMenuInicial.html" class="nav-link px-2 link-dark">Início</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">Contatos</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">Suporte Técnico</a></li>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <button class="navbar-toggler text-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button> 
+        <a class="navbar-brand" href="#">
+            <img src="../../img/logo.png" alt="mdo" width="52" height="52" class="d-inline-block align-text-top">
+        </a>            
+        
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contatos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Suporte Técnico</a>
+                </li>
             </ul>
-
-            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                <input type="search" class="form-control" placeholder="Procurar..." aria-label="Search">
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">                
             </form>
-
-            <div class="dropdown text-end">
-                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-                </a>
-                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                    <li><a class="dropdown-item" href="..\..\SiteOuvidoriaJau\Perfil\TelaPerfil.html">Perfil</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="..\..\SiteOuvidoriaJau\Login\TelaLogin.html">Sair</a></li>
-                </ul>
-            </div>
         </div>
+        <!-- Dropdown do User -->
+        <div class="navbar-brand dropdown text-end p-2 ">
+            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+            </a>
+            <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+                <li><a class="dropdown-item" href="..\..\SiteOuvidoriaJau\Perfil\TelaPerfil.html">Perfil</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="..\..\SiteOuvidoriaJau\Login\TelaLogin.html">Sair</a></li>
+            </ul>
+        </div>
+       
     </div>
-</header>
+</nav>
