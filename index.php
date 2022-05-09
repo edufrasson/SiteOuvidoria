abb368a70d149b8430e1d2f029ddffa3d5721982
edufrasson <edudomine2016@gmail.com>
2022-05-09 14:37:44 -0300

    adição do arquivo de rotas

--- index.php
+++ index.php
@@ -1,37 +1,6 @@
 <?php
 
-    $uri_parse = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-    switch($uri_parse){
-        case '/home':
-            include 'View/modules/Home/Home.php';
-        break;
-
-        case '/login':
-            include 'View/modules/Login/Login.php';
-        break;
-
-        case '/estatisticas':
-            include 'View/modules/Estatisticas/Estatisticas.php';
-        break;
-
-        case '/historico':
-            include 'View/modules/Historico/Historico.php';
-        break;
-
-        case '/meu_perfil':
-            include 'View/modules/MeuPerfil/MeuPerfil.php';
-        break;
-
-        case '/responder/denuncia':
-            include 'View/modules/Respostas/ResponderDenuncia.php';
-        break;
-
-        case '/responder/sugestao':
-            include 'View/modules/Respostas/ResponderSugestao.php';
-        break;
-
-        default:
-            header("Location: /home");
-        break;
-    }
+$uri_parse = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+include 'Autoload.php';
+include 'rotas.php';
+    
