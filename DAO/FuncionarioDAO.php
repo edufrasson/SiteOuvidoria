6a3b04c74b6b025540318a4dc75b0c71a37ca2de
gaboliveiradev <g38789138@gmail.com>
2022-05-09 18:49:19 -0300

    alteração na forma de conexão ao MySql

--- DAO/FuncionarioDAO.php
+++ DAO/FuncionarioDAO.php
@@ -4,20 +4,11 @@
         public $conexao;
         public function __construct()
         {
-            DEFINE("HOST", "metoda.com.br");
-            DEFINE("BANCO", "metodaco_ouvidoria");
-            DEFINE("USER", "metodaco_alunos");
-            DEFINE("PASS", "jau@2022");
-            
-            try {
-                $this->conexao = new PDO("mysql:host=".HOST.";dbname=".BANCO, USER, PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-                $this->conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (Exception $err) {
-                echo "[X] - Foi detectado um erro ao tentar se conectar ao banco de dados MySql80. <br>";
-                echo "<p>$err</p>";
-            }
+            include_once 'MySQL.php';
+    
+            $this->conexao = new MySQL();
         }
-
+        
         function insertFuncionario(FuncionarioModel $model) {
 
             $sql = "INSERT INTO Funcionario (nome, senha, email) VALUES (?, ?, ?)";
