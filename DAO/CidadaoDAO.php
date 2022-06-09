e6371abc82d21189c1a8122ffcdd3b14afde5a3e
gaboliveiradev <g38789138@gmail.com>
2022-06-09 06:31:58 -0300

    implementando a busca por cpf dos cidadãos

--- DAO/CidadaoDAO.php
+++ DAO/CidadaoDAO.php
@@ -22,11 +22,12 @@
 
             $sql = "SELECT nome, cpf, email, telefone, sexo, 
                     date_format(data_cadastro, '%d/%m/%Y') AS data_cadastro 
-                    FROM Cidadao WHERE nome LIKE ? OR email LIKE ?";
+                    FROM Cidadao WHERE nome LIKE ? OR email LIKE ? OR cpf LIKE ?";
     
             $stmt = $this->conexao->prepare($sql);
             $stmt->bindValue(1, $dados);
-            $stmt->bindValue(2, $dados);            
+            $stmt->bindValue(2, $dados);   
+            $stmt->bindValue(3, $dados);           
             $stmt->execute();
     
             return $stmt->fetchAll(PDO::FETCH_CLASS);
